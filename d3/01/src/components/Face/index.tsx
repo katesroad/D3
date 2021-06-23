e559cf8a4665f39794458d453d08ea2941c0fea2
Kate <test@gmail.com>
2021-06-22 22:49:33 -0400

    Face

--- d3/01/src/components/Face/index.tsx
+++ d3/01/src/components/Face/index.tsx
@@ -4,6 +4,8 @@ import { select } from 'd3'
 export const Face = () => {
   React.useEffect(() => {
     const svg = select('svg')
+    svg.attr('height', 400)
+    svg.attr('width', 400)
     const circle = svg.append('circle')
     //   attr, set up attribute for a svg
     circle.attr('r', 100).attr('cx', 200).attr('cy', 200)
